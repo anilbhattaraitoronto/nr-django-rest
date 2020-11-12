928747f7da392fdc9bb1891ff1fd463039d64b77
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-11-12 10:14:17 -0500

    created a component for postitem that is used by other components

--- client/src/components/pages/Home.svelte
+++ client/src/components/pages/Home.svelte
@@ -1,5 +1,6 @@
 <script>
     import { posts, newPosts, postTitles } from "../../stores/postStore.js";
+    import PostItem from "../shared/PostItem.svelte";
 </script>
 
 <style>
@@ -26,70 +27,7 @@
             <h2 class="title is-3 pl-3 has-text-centered">Latest</h2>
             <div class="container columns is-multiline  ">
                 {#each $newPosts as post}
-                    <div class=" column is-half">
-                        <div class="m-1">
-                            <div class="card p-0 m-0">
-                                {#if post.imageurl}
-                                    <div class="card-image">
-                                        <figure class="image is-4by3">
-                                            <img src={post.imageurl} alt="" />
-                                        </figure>
-                                    </div>
-                                {:else if post.thumbnail}
-                                    <div class="card-image">
-                                        <figure class="image is-4by3">
-                                            <img src={post.thumbnail} alt="" />
-                                        </figure>
-                                    </div>
-                                {/if}
-
-                                <div class="post card-content">
-                                    <p
-                                        class="pb-1 is-size-7 is-italic has-text-grey">
-                                        {new Date(post.posted_date).toDateString()}
-                                        /
-                                        <a
-                                            href="#/categoryposts/{post.category}"
-                                            class="has-text-black">#{post.category}</a>
-                                    </p>
-
-                                    <h3 class="title">
-                                        <a
-                                            href="#/{post.id}/{post.category}/{post.slug}"
-                                            class="has-text-weight-bold has-text-dark is-size-3">{post.title}</a>
-                                    </h3>
-                                    <h4
-                                        class="has-text-weight-bold has-text-black pb-4">
-                                        {post.author}
-                                    </h4>
-
-                                    <p class="has-text-grey is-italic">
-                                        {post.summary.substring(0, 50)}
-                                        ...
-                                        <a
-                                            href="#/{post.id}/{post.category}/{post.slug}"
-                                            class="has-text-weight-bold is-link">Read
-                                            full article</a>
-                                    </p>
-
-                                    <p>
-                                        <span
-                                            class="button is-small is-warning">Topics:
-                                        </span>
-
-                                        {#each post.topics as topic}
-                                            <a
-                                                href="#/topicposts/{topic.name}"
-                                                class="">{topic.name}
-                                                |
-                                            </a>
-                                        {/each}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End of post column -->
+                    <PostItem {post} />
                 {/each}
             </div>
         </article>
